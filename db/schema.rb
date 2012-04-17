c8b9a515a851db6a296af96b8399d02fe25f643a
james <tradervicsg@gmail.com>
2012-04-17 17:37:38 +0800

    newmlm

--- db/schema.rb
+++ db/schema.rb
@@ -89,11 +89,11 @@ ActiveRecord::Schema.define(:version => 20120314155306) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",                         :null => false
-    t.datetime "updated_at",                         :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
     t.string   "password_digest"
     t.string   "remember_token"
-    t.boolean  "admin",           :default => false
+    t.string   "admin"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
